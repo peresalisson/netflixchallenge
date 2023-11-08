1a5f94b9587a53d623f19210f1d72415b0cf3c5d
Alisson Peres Santos <alisson.santos@glintt.com>
2023-11-08 22:26:12 +0000

    C# solution

---
+++ netflixchallenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static char FirstUniqChar(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            char currentChar = s[i];
+            bool isUnique = true;
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (i != j && currentChar == s[j])
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique)
+            {
+                return currentChar;
+            }
+        }
+        return '_';
+    }
+
+    static void Main()
+    {
+        string s = "abacabad";
+        //string u = "aabbccddeeffgghh";
+        char result = FirstUniqChar(s);
+        Console.WriteLine($"FirstUniqChar(s) = '{result}'");
+    }
+}
